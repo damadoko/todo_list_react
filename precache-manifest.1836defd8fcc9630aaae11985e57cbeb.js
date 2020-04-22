4432cae17b4cca05b3295d7e1e7f0b44e933bba0
Le Minh Triet <leminhtriet@Les-MacBook-Pro.local>
2020-04-22 14:18:23 +0700

    Updates

---
+++ precache-manifest.1836defd8fcc9630aaae11985e57cbeb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c07c6bf5f7d5278a3dd07128d15e35",
+    "url": "/todo_list_react/index.html"
+  },
+  {
+    "revision": "443de2005f7afc08edf6",
+    "url": "/todo_list_react/static/css/main.16b63477.chunk.css"
+  },
+  {
+    "revision": "eb0825042c15c123020a",
+    "url": "/todo_list_react/static/js/2.d0d95f7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_list_react/static/js/2.d0d95f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443de2005f7afc08edf6",
+    "url": "/todo_list_react/static/js/main.06a3a20c.chunk.js"
+  },
+  {
+    "revision": "4486eec8ca7ca12f2b3c",
+    "url": "/todo_list_react/static/js/runtime-main.e854c5de.js"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/todo_list_react/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/todo_list_react/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "/todo_list_react/static/media/Montserrat-Italic.a7063e0c.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/todo_list_react/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/todo_list_react/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/todo_list_react/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
